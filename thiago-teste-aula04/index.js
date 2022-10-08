096dbb543f0df8b90a1ffc9f4720b30535960640
thiago <thiagodesouza.dev@gmail.com>
2022-10-08 19:48:12 -0300

    css

--- thiago-teste-aula04/index.js
+++ thiago-teste-aula04/index.js
@@ -16,65 +16,96 @@ const criarIngrediente = document.querySelector("#criar-ingrediente");
 const listaIngredientes = document.querySelector("#lista-ingredientes");
 const butonRemoverFinalizados = document.querySelector("#remover-finalizados");
 const clearAllTasks = document.querySelector("#apaga-tudo");
+const mostrarMais = document.querySelector("#mostrar-mais");
+const todasAsReceitas = document.querySelector("#todas-as-receitas");
 const fetchCurrency = () => __awaiter(void 0, void 0, void 0, function* () {
+    loading();
     const dataResults = yield fetch("https://receitas-server.vercel.app/api")
         .then((response) => response.json())
         .then((data) => data);
     return dataResults;
 });
+let number = 5;
 const render = (objeto) => __awaiter(void 0, void 0, void 0, function* () {
     const data = yield objeto;
     if (rootElement) {
         rootElement.innerHTML = "";
-        data.slice(0, 5).forEach((item) => {
+        data === null || data === void 0 ? void 0 : data.slice(0, number).forEach((item) => {
             rootElement.innerHTML += `
-      <div>
-          <h1>Autor da receita ${item.Author}</h1>
+      <div class="card">
+          <h1 class="card-title">Autor da receita ${item.Author}</h1>
           <img src="${item.urlImage}" alt="${item.Name}">
-          <p>${item.Name}</p>
+          <h1>${item.Name}</h1>
           <details>
             <summary>ingredientes</summary>
             <p>${item.Ingredients}</p>
-            </details>
+          </details>
+          <details>
+            <summary>Modo de preparo</summary>
             <h2>${item.Method}</h2>
-
+          </details>
       </div>
         `;
         });
     }
 });
+const choices = [];
+const ingredientElement = document.querySelectorAll(".ingredient");
+ingredientElement.forEach(ingredient => {
+    ingredient.addEventListener("change", () => __awaiter(void 0, void 0, void 0, function* () {
+        const data = yield fetchCurrency();
+        console.log(data);
+        ingredient.checked ?
+            choices.push(ingredient.value)
+            : choices.splice(choices.indexOf(ingredient.value), 1);
+        console.log(choices);
+        const filteredArray = data.filter(item => {
+            for (let index = 0; index < choices.length; index++) {
+                const element = choices[index];
+                return item.Ingredients.includes(element.toLowerCase());
+            }
+        });
+        console.log(filteredArray, "filtered");
+        render(filteredArray);
+    }));
+});
 const search = (objeto, event) => __awaiter(void 0, void 0, void 0, function* () {
-    const array = [];
-    const checkboxes = document.querySelectorAll("input[type=checkbox]:checked");
-    for (let i = 0; i < checkboxes.length; i++) {
-        array.push(checkboxes[i].value);
-    }
     event === null || event === void 0 ? void 0 : event.preventDefault();
+    // const array: string[] = [];
+    // const checkboxes = document.querySelectorAll("input[type=checkbox]:checked");
+    // console.log(array);
+    // for (let i = 0; i < checkboxes.length; i++) {
+    //   array.push(checkboxes[i].value);
+    // }
     const data = yield objeto;
+    // const ingredientString = "";
+    // for (let index = 0; index < array.length; index++) {
+    //   ingredientString += `${array[index]}`;
+    // }
+    // console.log(ingredientString, "string");
     const searchInputValue = searchInputElement.value;
-    for (let index = 0; index < array.length; index++) {
-        const element = array[index];
-        const dataResult = data.filter((item) => item.Name.toLowerCase().includes(searchInputValue.toLowerCase()) &&
-            item.Ingredients[index].toLowerCase().includes(element));
-        yield render(dataResult);
-    }
+    const dataResult = data.filter((item) => item.Name.toLowerCase().includes(searchInputValue.toLowerCase())
+    //   &&
+    // item.Ingredients.includes(ingredientString)
+    );
+    render(dataResult);
 });
 searchButtonElement === null || searchButtonElement === void 0 ? void 0 : searchButtonElement.addEventListener("click", (event) => search(fetchCurrency(), event));
 criarIngrediente === null || criarIngrediente === void 0 ? void 0 : criarIngrediente.addEventListener("click", () => {
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     checkbox.id = textoIngrediente === null || textoIngrediente === void 0 ? void 0 : textoIngrediente.value;
-    checkbox.setAttribute("class", "ingredientChecked");
+    checkbox.setAttribute("class", "ingredient");
     checkbox.name = "interest";
     checkbox.value = textoIngrediente === null || textoIngrediente === void 0 ? void 0 : textoIngrediente.value;
     const label = document.createElement("label");
     label.htmlFor = textoIngrediente === null || textoIngrediente === void 0 ? void 0 : textoIngrediente.value;
     label.appendChild(document.createTextNode(textoIngrediente === null || textoIngrediente === void 0 ? void 0 : textoIngrediente.value));
-    const br = document.createElement("br");
+    // const br = document.createElement("br");
     const container = document.getElementById("lista-ingredientes");
     container === null || container === void 0 ? void 0 : container.appendChild(checkbox);
     container === null || container === void 0 ? void 0 : container.appendChild(label);
-    container === null || container === void 0 ? void 0 : container.appendChild(br);
+    // container?.appendChild(br);
 });
 listaIngredientes === null || listaIngredientes === void 0 ? void 0 : listaIngredientes.addEventListener("dblclick", (event) => {
     const evt = event.target;
@@ -96,4 +127,20 @@ butonRemoverFinalizados === null || butonRemoverFinalizados === void 0 ? void 0 
         listaIngredientes === null || listaIngredientes === void 0 ? void 0 : listaIngredientes.removeChild(completo[index]);
     }
 });
+todasAsReceitas === null || todasAsReceitas === void 0 ? void 0 : todasAsReceitas.addEventListener("click", () => {
+    render(fetchCurrency());
+});
+mostrarMais === null || mostrarMais === void 0 ? void 0 : mostrarMais.addEventListener("click", (event) => {
+    event.preventDefault();
+    number += 10;
+    render(fetchCurrency());
+});
+const loading = () => {
+    const div = document.createElement("div");
+    rootElement.appendChild(div);
+    div.className = "loader";
+};
+const removeLoading = () => {
+    document.querySelector(".loader").remove();
+};
 render(fetchCurrency());
--- thiago-teste-aula04/index.ts
+++ thiago-teste-aula04/index.ts
@@ -16,56 +16,88 @@ const criarIngrediente = document.querySelector("#criar-ingrediente");
 const listaIngredientes = document.querySelector("#lista-ingredientes");
 const butonRemoverFinalizados = document.querySelector("#remover-finalizados");
 const clearAllTasks = document.querySelector("#apaga-tudo");
+const mostrarMais= document.querySelector("#mostrar-mais");
+const todasAsReceitas= document.querySelector("#todas-as-receitas");
 
 const fetchCurrency = async  () => {
+  loading();
   const dataResults: IRecipe = await fetch("https://receitas-server.vercel.app/api")
     .then((response) => response.json() )
     .then((data) =>  data);
   return dataResults;
 };
 
+let number = 5;
 const render = async (objeto: IRecipe) => {
   const data: IRecipe = await objeto;
   if (rootElement) {
     rootElement.innerHTML = "";
-    data.slice(0, 5).forEach((item: IRecipe) => { 
+    data?.slice(0, number).forEach((item: IRecipe) => { 
       rootElement.innerHTML += `
-      <div>
-          <h1>Autor da receita ${item.Author}</h1>
+      <div class="card">
+          <h1 class="card-title">Autor da receita ${item.Author}</h1>
           <img src="${item.urlImage}" alt="${item.Name}">
-          <p>${item.Name}</p>
+          <h1>${item.Name}</h1>
           <details>
             <summary>ingredientes</summary>
             <p>${item.Ingredients}</p>
-            </details>
+          </details>
+          <details>
+            <summary>Modo de preparo</summary>
             <h2>${item.Method}</h2>
-
+          </details>
       </div>
         `;
     });
   }
 };
+const choices: string[] = [];
+const ingredientElement = document.querySelectorAll(".ingredient");
 
-const search = async (objeto: IRecipe, event: Event) => {
-  const array: string[] = [];
-  const checkboxes = document.querySelectorAll("input[type=checkbox]:checked");
-
-  for (let i = 0; i < checkboxes.length; i++) {
-    array.push(checkboxes[i].value);
-  }
-  
-  event?.preventDefault();
-  const data: IRecipe = await objeto;
-  const searchInputValue = (searchInputElement as HTMLInputElement).value;
-  for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    const dataResult =  data.filter((item: IRecipe) => 
-      item.Name.toLowerCase().includes(searchInputValue.toLowerCase()) &&
-      
-      item.Ingredients[index].toLowerCase().includes(element)
+ingredientElement.forEach(ingredient => {
+  ingredient.addEventListener("change", async () => {
+    const data = await fetchCurrency(); 
+    console.log(data);
+    
+    ingredient.checked ? 
+      choices.push(ingredient.value)
+      : choices.splice( choices.indexOf(ingredient.value), 1 );
+    console.log(choices);
+    const filteredArray = data.filter(item => {
+      for (let index = 0; index < choices.length; index++) {
+        const element = choices[index];
+        return item.Ingredients.includes(element.toLowerCase());
+      }}
     );
-    await render(dataResult);
-  }
+    console.log(filteredArray, "filtered");
+    render(filteredArray);
+  });
+});
+const search = async (objeto: IRecipe, event: Event) => {
+  event?.preventDefault();
+  // const array: string[] = [];
+  // const checkboxes = document.querySelectorAll("input[type=checkbox]:checked");
+  // console.log(array);
+
+  // for (let i = 0; i < checkboxes.length; i++) {
+  //   array.push(checkboxes[i].value);
+  // }
+  
+  const data: IRecipe = await objeto;
+  // const ingredientString = "";
+  
+  // for (let index = 0; index < array.length; index++) {
+    
+  //   ingredientString += `${array[index]}`;
+  // }
+  // console.log(ingredientString, "string");
+  const searchInputValue = (searchInputElement as HTMLInputElement).value;
+  const dataResult =  data.filter((item: IRecipe) => 
+    item.Name.toLowerCase().includes(searchInputValue.toLowerCase()) 
+    //   &&
+    // item.Ingredients.includes(ingredientString)
+  );
+  render(dataResult);
 };
 
 (searchButtonElement as HTMLButtonElement)?.addEventListener("click", (event) => search(fetchCurrency(), event));
@@ -74,7 +106,7 @@ criarIngrediente?.addEventListener("click", () => {
   const checkbox: HTMLInputElement = document.createElement("input");
   checkbox.type = "checkbox";
   checkbox.id = textoIngrediente?.value;
-  checkbox.setAttribute("class", "ingredientChecked");
+  checkbox.setAttribute("class", "ingredient");
   checkbox.name = "interest";
   checkbox.value = textoIngrediente?.value;
  
@@ -82,12 +114,12 @@ criarIngrediente?.addEventListener("click", () => {
   label.htmlFor = textoIngrediente?.value;
   label.appendChild(document.createTextNode(textoIngrediente?.value));
  
-  const br = document.createElement("br");
+  // const br = document.createElement("br");
  
   const container = document.getElementById("lista-ingredientes");
   container?.appendChild(checkbox);
   container?.appendChild(label);
-  container?.appendChild(br);
+  // container?.appendChild(br);
 });
 
 (listaIngredientes as HTMLButtonElement)?.addEventListener("dblclick", (event) => {
@@ -111,5 +143,23 @@ criarIngrediente?.addEventListener("click", () => {
     listaIngredientes?.removeChild(completo[index]);
   }
 });
+(todasAsReceitas as HTMLButtonElement)?.addEventListener("click", () => {
+  render(fetchCurrency());
+});
+
+mostrarMais?.addEventListener("click", (event) => {
+  event.preventDefault();
+  number += 10;
+  render(fetchCurrency());
+});
+const loading = () => {
+  const div = document.createElement("div");
+  rootElement.appendChild(div);
+  div.className = "loader";
+};
+
+const removeLoading = () => {
+  document.querySelector(".loader").remove();
+}; 
 
 render(fetchCurrency());
